05428a4e64f3bc47882b672f1f9f7c3a3313c7c5
marcwebbie <marcwebbie@gmail.com>
2013-06-28 10:05:05 -0300

    Fixed missing alias variable

--- sshalias.py
+++ sshalias.py
@@ -2,23 +2,28 @@
 import argparse
 from os.path import expanduser, join
 
-
+# Parse console arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("user", help="User on the SSH server")
+parser.add_argument("alias", help="alias to the SSH server")
+parser.add_argument("user", help="user on the SSH server")
 parser.add_argument("host", help="SSH server address")
-parser.add_argument("-v","--verbose", help="Verbose output", action="store_true")
+parser.add_argument("-v","--verbose", help="verbose output", action="store_true")
+parser.add_argument("-t","--test", help="redirect output to standard stream", 
+                    action="store_true")
 args = parser.parse_args()
 
+# Build file
+config_file_path = join(expanduser("~"), '.ssh/config')
 sshconfig_text = '''
-Host example
+Host {}
   User {}
   HostName {}
-'''.format(args.user, args.host)
+'''.format(args.alias, args.user, args.host)
 
-config_file_path = join(expanduser("~"), '.ssh/config')
-with open(config_file_path, 'w+') as fb:
-    fb.write(sshconfig_text)
+if not args.test:
+    with open(config_file_path, 'w+') as fb:
+        fb.write(sshconfig_text)
 
-if args.verbose:
+if args.verbose or args.test:
     print(config_file_path)
     print(sshconfig_text)
